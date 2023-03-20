9474cde1b4c37672f97b88fa5d81261129e86807
Crawsoir <werq10.fifty@gmail.com>
2023-03-20 00:15:22 -0400

    Bous

--- TBD-dragon-game/scenes/forest/forest.gd
+++ TBD-dragon-game/scenes/forest/forest.gd
@@ -7,6 +7,8 @@ extends Node2D
 var camera_left_limit = 0
 var camera_right_limit = 13700
 
+var boss_beaten = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -20,3 +22,11 @@ func _ready():
 func _on_DetectionArea2D1_body_entered(body):
 	if body.get_name() == "Player":
 		Global.move_to_area("CAVE", "SpawnMarkers/Spawn1")
+
+
+func _on_BossInitiator_body_entered(body):
+	if Global.quest_progress["gate_quest"]["Status"] != Global.DONE_QUEST:
+		if $BossGate/BossGate.state == $BossGate/BossGate.OPEN:
+			$BossGate/BossGate.state = $BossGate/BossGate.CLOSING
+			Global.get_dialogue("Boss1.json")
+			
