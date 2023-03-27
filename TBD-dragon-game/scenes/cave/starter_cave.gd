f3cf491cd07f4b473cee7a4bea86d73c5e7b283d
Crawsoir <werq10.fifty@gmail.com>
2023-03-27 02:20:47 -0400

    Add music

--- TBD-dragon-game/scenes/cave/starter_cave.gd
+++ TBD-dragon-game/scenes/cave/starter_cave.gd
@@ -6,7 +6,7 @@ var camera_right_limit = 3326
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AudioStreamPlayer.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- TBD-dragon-game/scenes/forest/forest.gd
+++ TBD-dragon-game/scenes/forest/forest.gd
@@ -13,7 +13,7 @@ var boss = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$AudioStreamPlayer.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
