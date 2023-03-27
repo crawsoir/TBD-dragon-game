2326cb9dece04e0a163796555637a739ce6177c5
Emily Wu <chinadragon.emily@gmail.com>
2023-03-26 20:36:17 -0400

    Fix Confirmation Box Issues with spawning too many of itself by pausing the scene tree while the box is open

--- TBD-dragon-game/gui/ConfirmationBox.gd
+++ TBD-dragon-game/gui/ConfirmationBox.gd
@@ -1,8 +1,14 @@
 extends CanvasLayer
 
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	get_tree().paused = true
+
 func _on_Yes_pressed():
 	Global.move_to_area("CAVE","DefaultSpawn")
+	get_tree().paused = false
 	queue_free()
 
 func _on_No_pressed():
+	get_tree().paused = false
 	queue_free()
